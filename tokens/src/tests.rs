0289c108d05d45ab0236bb31aea5ac15422aab98
Shaopeng Wang <spxwang@gmail.com>
2019-11-05 15:32:57 +1300

    Tokens module unit tests.

---
+++ tokens/src/tests.rs
@@ -0,0 +1,74 @@
+//! Unit tests for the tokens module.
+
+#![cfg(test)]
+
+use super::*;
+use mock::{Balance, ExtBuilder, System, TestEvent, Tokens, ALICE, BOB, TEST_TOKEN_ID};
+use srml_support::{assert_noop, assert_ok};
+
+#[test]
+fn genesis_issuance_should_work() {
+	ExtBuilder::default()
+		.one_hundred_for_alice_n_bob()
+		.build()
+		.execute_with(|| {
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &ALICE), 100);
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &BOB), 100);
+			assert_eq!(Tokens::total_issuance(TEST_TOKEN_ID), 200);
+		});
+}
+
+#[test]
+fn transfer_should_work() {
+	ExtBuilder::default()
+		.one_hundred_for_alice_n_bob()
+		.build()
+		.execute_with(|| {
+			assert_ok!(Tokens::transfer(Some(ALICE).into(), BOB, TEST_TOKEN_ID, 50));
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &ALICE), 50);
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &BOB), 150);
+			assert_eq!(Tokens::total_issuance(TEST_TOKEN_ID), 200);
+
+			let transferred_event = TestEvent::tokens(RawEvent::Transferred(TEST_TOKEN_ID, ALICE, BOB, 50));
+			assert!(System::events().iter().any(|record| record.event == transferred_event));
+
+			assert_noop!(
+				Tokens::transfer(Some(ALICE).into(), BOB, TEST_TOKEN_ID, 60),
+				"balance too low to transfer",
+			);
+		});
+}
+
+#[test]
+fn deposit_should_work() {
+	ExtBuilder::default()
+		.one_hundred_for_alice_n_bob()
+		.build()
+		.execute_with(|| {
+			assert_ok!(Tokens::deposit(TEST_TOKEN_ID, &ALICE, 100));
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &ALICE), 200);
+			assert_eq!(Tokens::total_issuance(TEST_TOKEN_ID), 300);
+
+			assert_noop!(
+				Tokens::deposit(TEST_TOKEN_ID, &ALICE, Balance::max_value()),
+				"total issuance overflow after deposit",
+			);
+		});
+}
+
+#[test]
+fn withdraw_should_work() {
+	ExtBuilder::default()
+		.one_hundred_for_alice_n_bob()
+		.build()
+		.execute_with(|| {
+			assert_ok!(Tokens::withdraw(TEST_TOKEN_ID, &ALICE, 50));
+			assert_eq!(Tokens::balance(TEST_TOKEN_ID, &ALICE), 50);
+			assert_eq!(Tokens::total_issuance(TEST_TOKEN_ID), 150);
+
+			assert_noop!(
+				Tokens::withdraw(TEST_TOKEN_ID, &ALICE, 60),
+				"balance too low to withdraw",
+			);
+		});
+}
