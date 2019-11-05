a9172d4939692a6ffe4ecf9d384641cb0ca64719
Shaopeng Wang <spxwang@gmail.com>
2019-11-05 16:49:35 +1300

    Remove allow dead code.

--- tokens/src/mock.rs
+++ tokens/src/mock.rs
@@ -83,7 +83,6 @@ impl Default for ExtBuilder {
 }
 
 impl ExtBuilder {
-	#[allow(dead_code)]
 	pub fn balances(mut self, account_ids: Vec<AccountId>, initial_balance: Balance) -> Self {
 		self.endowed_accounts = account_ids;
 		self.initial_balance = initial_balance;
