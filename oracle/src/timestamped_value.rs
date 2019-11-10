bf2cec23de03934c8730215fd2845a1e571b891f
Ermal Kaleci <ermalkaleci@gmail.com>
2019-11-10 09:09:16 +0100

    cleanup

--- oracle/src/timestamped_value.rs
+++ oracle/src/timestamped_value.rs
@@ -1,7 +1,7 @@
 use codec::{Decode, Encode};
 use sr_primitives::RuntimeDebug;
 
-#[derive(Encode, Decode, Default, RuntimeDebug, Eq, PartialEq, Clone)]
+#[derive(Encode, Decode, RuntimeDebug, Eq, PartialEq, Clone)]
 pub struct TimestampedValue<Value, Moment> {
 	pub value: Value,
 	pub timestamp: Moment,
