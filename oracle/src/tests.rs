a78399659ac88ce8078fc495c0c47bf4dcd55f88
Bryan Chen <xlchen1291@gmail.com>
2019-11-21 17:18:00 +1300

    fix tests

--- oracle/src/tests.rs
+++ oracle/src/tests.rs
@@ -6,7 +6,7 @@ use crate::TimestampedValue;
 use palette_support::assert_ok;
 
 #[test]
-fn should_feed_data() {
+fn should_feed_value() {
 	new_test_ext().execute_with(|| {
 		let key: u32 = 1;
 		let account_id: u64 = 1;
@@ -18,10 +18,48 @@ fn should_feed_data() {
 			timestamp: 12345,
 		};
 
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(account_id), key, 1000));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(account_id), key, 1000));
 
 		let feed_data = ModuleOracle::raw_values(&key, &account_id).unwrap();
 		assert_eq!(feed_data, expected);
+	});
+}
+
+#[test]
+fn should_feed_values() {
+	new_test_ext().execute_with(|| {
+		let account_id: u64 = 1;
+
+		Timestamp::set_timestamp(12345);
+
+		assert_ok!(ModuleOracle::feed_values(
+			Origin::signed(account_id),
+			vec![(1, 1000), (2, 900), (3, 800)]
+		));
+
+		assert_eq!(
+			ModuleOracle::raw_values(&1, &account_id),
+			Some(TimestampedValue {
+				value: 1000,
+				timestamp: 12345,
+			})
+		);
+
+		assert_eq!(
+			ModuleOracle::raw_values(&2, &account_id),
+			Some(TimestampedValue {
+				value: 900,
+				timestamp: 12345,
+			})
+		);
+
+		assert_eq!(
+			ModuleOracle::raw_values(&3, &account_id),
+			Some(TimestampedValue {
+				value: 800,
+				timestamp: 12345,
+			})
+		);
 	});
 }
 
@@ -30,7 +68,7 @@ fn should_change_status_when_feeding() {
 	new_test_ext().execute_with(|| {
 		let key: u32 = 1;
 		assert_eq!(ModuleOracle::has_update(key), false);
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(1), key, 1000));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(1), key, 1000));
 		assert_eq!(ModuleOracle::has_update(key), true);
 	});
 }
@@ -56,8 +94,8 @@ fn should_read_raw_values() {
 			},
 		];
 
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(1), key, 1000));
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(2), key, 1200));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(1), key, 1000));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(2), key, 1200));
 
 		let raw_values = ModuleOracle::read_raw_values(&key);
 		assert_eq!(raw_values, expected);
@@ -76,9 +114,9 @@ fn should_combined_data() {
 
 		let key: u32 = 1;
 
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(1), key, 1300));
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(2), key, 1000));
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(3), key, 1200));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(1), key, 1300));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(2), key, 1000));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(3), key, 1200));
 		assert_eq!(ModuleOracle::get(&key), expected);
 	});
 }
@@ -95,9 +133,9 @@ fn should_return_prev_value() {
 
 		let key: u32 = 1;
 
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(1), key, 1300));
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(2), key, 1000));
-		assert_ok!(ModuleOracle::feed_data(Origin::signed(3), key, 1200));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(1), key, 1300));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(2), key, 1000));
+		assert_ok!(ModuleOracle::feed_value(Origin::signed(3), key, 1200));
 		assert_eq!(ModuleOracle::get(&key), expected);
 
 		Timestamp::set_timestamp(23456);
