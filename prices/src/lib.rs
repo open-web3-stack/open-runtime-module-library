88d3d15580dd5ed4f1c61ab47f0d843fcf19214f
Bryan Chen <xlchen1291@gmail.com>
2020-04-14 18:08:46 +1200

    refactor prices module

--- prices/src/lib.rs
+++ prices/src/lib.rs
@@ -1,35 +1,26 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_module, decl_storage, Parameter};
+use frame_support::Parameter;
 use orml_traits::{DataProvider, PriceProvider};
 use orml_utilities::FixedU128;
 use sp_runtime::traits::{MaybeSerializeDeserialize, Member};
+use sp_std::marker::PhantomData;
 
 pub type Price = FixedU128;
 
-pub trait Trait: frame_system::Trait {
-	type CurrencyId: Parameter + Member + Copy + MaybeSerializeDeserialize;
-	type Source: DataProvider<Self::CurrencyId, Price>;
-}
-
-mod mock;
 mod tests;
 
-decl_storage! {
-	trait Store for Module<T: Trait> as Prices { }
-}
+pub struct DefaultPriceProvider<CurrencyId, Source>(PhantomData<(CurrencyId, Source)>);
 
-decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin { }
-}
+impl<CurrencyId, Source> PriceProvider<CurrencyId, Price> for DefaultPriceProvider<CurrencyId, Source>
+where
+	CurrencyId: Parameter + Member + Copy + MaybeSerializeDeserialize,
+	Source: DataProvider<CurrencyId, Price>,
+{
+	fn get_price(base_currency_id: CurrencyId, quote_currency_id: CurrencyId) -> Option<Price> {
+		let base_price = Source::get(&base_currency_id)?;
+		let quote_price = Source::get(&quote_currency_id)?;
 
-impl<T: Trait> Module<T> {}
-
-impl<T: Trait> PriceProvider<T::CurrencyId, Price> for Module<T> {
-	fn get_price(base_currency_id: T::CurrencyId, quote_currency_id: T::CurrencyId) -> Option<Price> {
-		let base_price = T::Source::get(&base_currency_id)?;
-		let quote_price = T::Source::get(&quote_currency_id)?;
-
-		quote_price.checked_div(&base_price)
+		base_price.checked_div(&quote_price)
 	}
 }
