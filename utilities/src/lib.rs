3fb42b8bdf7695bff76b2107210250f34078ea28
zjb0807 <zjb0807@qq.com>
2020-04-23 15:38:05 +0800

    remove Fixed128

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -1,9 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod fixed_128;
 pub mod fixed_u128;
 pub mod linked_item;
 
-pub use fixed_128::Fixed128;
 pub use fixed_u128::FixedU128;
 pub use linked_item::{LinkedItem, LinkedList};
