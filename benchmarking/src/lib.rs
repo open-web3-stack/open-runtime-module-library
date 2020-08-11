3023e32b700edc739c97d3efaa1d240c8d8e71d0
Shaopeng Wang <spxwang@gmail.com>
2020-08-11 14:29:48 +1200

    make clippy happy

--- benchmarking/src/lib.rs
+++ benchmarking/src/lib.rs
@@ -136,15 +136,15 @@ pub use sp_runtime::traits::Zero;
 ///
 /// ```ignore
 /// sort_vector {
-/// 	let x in 1 .. 10000;
-/// 	let mut m = Vec::<u32>::new();
-/// 	for i in (0..x).rev() {
-/// 		m.push(i);
-/// 	}
+///     let x in 1 .. 10000;
+///     let mut m = Vec::<u32>::new();
+///     for i in (0..x).rev() {
+///         m.push(i);
+///     }
 /// }: {
-/// 	m.sort();
+///     m.sort();
 /// } verify {
-/// 	ensure!(m[0] == 0, "You forgot to sort!")
+///     ensure!(m[0] == 0, "You forgot to sort!")
 /// }
 /// ```
 ///
@@ -942,14 +942,14 @@ macro_rules! impl_benchmark_test {
 ///
 /// ```ignore
 /// let whitelist: Vec<Vec<u8>> = vec![
-/// 	// Block Number
-/// 	hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef702a5c1b19ab7a04f536c519aca4983ac").to_vec(),
-/// 	// Total Issuance
-/// 	hex_literal::hex!("c2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80").to_vec(),
-/// 	// Execution Phase
-/// 	hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef7ff553b5a9862a516939d82b3d3d8661a").to_vec(),
-/// 	// Event Count
-/// 	hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef70a98fdbe9ce6c55837576c60c7af3850").to_vec(),
+///     // Block Number
+///     hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef702a5c1b19ab7a04f536c519aca4983ac").to_vec(),
+///     // Total Issuance
+///     hex_literal::hex!("c2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80").to_vec(),
+///     // Execution Phase
+///     hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef7ff553b5a9862a516939d82b3d3d8661a").to_vec(),
+///     // Event Count
+///     hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef70a98fdbe9ce6c55837576c60c7af3850").to_vec(),
 /// ];
 ///
 /// Then define a mutable local variable to hold your `BenchmarkBatch` object:
