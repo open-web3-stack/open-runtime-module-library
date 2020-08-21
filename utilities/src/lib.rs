07fc40af8808db8b499ebeef0d91449c7a0fe0a8
Bryan Chen <xlchen1291@gmail.com>
2020-08-21 22:02:50 +1200

    disable with_transaction for now

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -16,14 +16,15 @@ pub use ordered_set::OrderedSet;
 /// Transactions can be nested to any depth. Commits happen to the parent
 /// transaction.
 pub fn with_transaction_result<R>(f: impl FnOnce() -> Result<R, DispatchError>) -> Result<R, DispatchError> {
-	with_transaction(|| {
-		let res = f();
-		if res.is_ok() {
-			TransactionOutcome::Commit(res)
-		} else {
-			TransactionOutcome::Rollback(res)
-		}
-	})
+	// with_transaction(|| {
+	// 	let res = f();
+	// 	if res.is_ok() {
+	// 		TransactionOutcome::Commit(res)
+	// 	} else {
+	// 		TransactionOutcome::Rollback(res)
+	// 	}
+	// })
+	f()
 }
 
 #[cfg(test)]
