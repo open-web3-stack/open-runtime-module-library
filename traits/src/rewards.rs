432ebafe52ea575d551956452ed4c2f61a16e33a
wangjj9219 <183318287@qq.com>
2020-09-05 01:01:48 +0800

    update RewardHandler trait (#274)

--- traits/src/rewards.rs
+++ traits/src/rewards.rs
@@ -1,6 +1,6 @@
 use codec::FullCodec;
 use sp_runtime::traits::{AtLeast32BitUnsigned, MaybeSerializeDeserialize};
-use sp_std::fmt::Debug;
+use sp_std::{fmt::Debug, vec::Vec};
 
 /// Hooks to manage reward pool
 pub trait RewardHandler<AccountId, BlockNumber> {
@@ -13,8 +13,14 @@ pub trait RewardHandler<AccountId, BlockNumber> {
 	/// The reward pool ID type
 	type PoolId: Copy + FullCodec;
 
+	/// The currency type
+	type CurrencyId: FullCodec + Eq + PartialEq + Copy + MaybeSerializeDeserialize + Debug;
+
 	/// Accumulate rewards
-	fn accumulate_reward(now: BlockNumber, callback: impl Fn(Self::PoolId, Self::Balance)) -> Self::Balance;
+	fn accumulate_reward(
+		now: BlockNumber,
+		callback: impl Fn(Self::PoolId, Self::Balance),
+	) -> Vec<(Self::CurrencyId, Self::Balance)>;
 
 	/// Payout the reward to `who`
 	fn payout(who: &AccountId, pool: Self::PoolId, amount: Self::Balance);
