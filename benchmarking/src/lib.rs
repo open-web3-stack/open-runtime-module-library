5a1ba6bcc67cd0c59a0b578df9bb8b144a78598e
zjb0807 <zjb0807@qq.com>
2020-09-16 14:33:19 +0800

    fix fmt

--- benchmarking/src/lib.rs
+++ benchmarking/src/lib.rs
@@ -628,7 +628,7 @@ macro_rules! benchmark_backend {
 // Every variant must implement [`BenchmarkingSetup`].
 //
 // ```nocompile
-//
+// 
 // struct Transfer;
 // impl BenchmarkingSetup for Transfer { ... }
 //
