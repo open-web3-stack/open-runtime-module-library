ed02b0aa09ba8d6ca7124b31e332bd2b641467c9
Bryan Chen <xlchen1291@gmail.com>
2020-09-24 18:27:50 +1200

    update

--- currencies/src/mock.rs
+++ currencies/src/mock.rs
@@ -113,7 +113,7 @@ impl Trait for Runtime {
 }
 pub type Currencies = Module<Runtime>;
 pub type NativeCurrency = NativeCurrencyOf<Runtime>;
-pub type AdaptedBasicCurrency = BasicCurrencyAdapter<PalletBalances, Balance, Balance, i64, u64>;
+pub type AdaptedBasicCurrency = BasicCurrencyAdapter<Runtime, PalletBalances, i64, u64>;
 
 pub const ALICE: AccountId = 1;
 pub const BOB: AccountId = 2;
