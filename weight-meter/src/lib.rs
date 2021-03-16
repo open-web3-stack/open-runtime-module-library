31166278dfeb662ae967034964e49717f5e0675b
Ermal Kaleci <ermalkaleci@gmail.com>
2021-03-16 19:51:35 +0100

    clippy

--- weight-meter/src/lib.rs
+++ weight-meter/src/lib.rs
@@ -31,7 +31,6 @@ pub use weight_meter_procedural::weight;
 /// #[orml_weight_meter::method_benchmarks]
 /// #[frame_support::pallet]
 /// pub mod module {
-/// 	use super::*;
 ///     ..
 /// }
 /// ```
@@ -74,7 +73,7 @@ pub use weight_meter_procedural::weight;
 ///     #[pallet::weight(T::WeightInfo::do_something())]
 ///     #[orml_weight_meter::start_with(1_000_000)]
 ///     pub fn do_something(origin: OriginFor<T>, something: u32) ->
-///		DispatchResultWithPostInfo {
+///	    DispatchResultWithPostInfo {
 ///         let who = ensure_signed(origin)?;
 ///         Self::inner_do_something(something);
 ///         // Emit an event.
