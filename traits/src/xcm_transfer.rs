ed01c3b90b1cff6e0ccf42a278b0aebf3dd6d0f5
Matthias Seitz <matthias.seitz@outlook.de>
2021-07-05 14:35:03 +0200

    declare XcmTransfer trait

---
+++ traits/src/xcm_transfer.rs
@@ -0,0 +1,28 @@
+use frame_support::dispatch::DispatchError;
+use frame_support::weights::Weight;
+use xcm::{
+	opaque::v0::{MultiAsset, MultiLocation},
+	v0::prelude::XcmError,
+};
+
+type XcmExecutionResult = sp_std::result::Result<Option<XcmError>, DispatchError>;
+
+/// Abstraction over cross-chain token transfers.
+pub trait XcmTransfer<AccountId, Balance, CurrencyId> {
+	/// Transfer native currencies.
+	fn transfer(
+		who: AccountId,
+		currency_id: CurrencyId,
+		amount: Balance,
+		dest: MultiLocation,
+		dest_weight: Weight,
+	) -> XcmExecutionResult;
+
+	/// Transfer `MultiAsset`
+	fn transfer_multi_asset(
+		who: AccountId,
+		asset: MultiAsset,
+		dest: MultiLocation,
+		dest_weight: Weight,
+	) -> XcmExecutionResult;
+}
