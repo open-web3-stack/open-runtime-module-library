cd6e9337263f80fee201c0e4027d4f4d0694cb07
Shaun Wang <spxwang@gmail.com>
2021-07-15 10:40:38 +1200

    Remove module excludes in run script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,10 +8,6 @@ shift
 set -x
 
 for file in **/Cargo.toml; do
-	if [ "$file" == "xtokens/Cargo.toml" ] || [ "$file" == "xcm-support/Cargo.toml" ] || [ "$file" == "unknown-tokens/Cargo.toml" ]
-		then
-			continue
-	fi
 	cargo $COMMAND --manifest-path "$file" $@;
 done
 
