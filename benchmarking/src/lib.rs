4bb3dd13cc9940432da859db854ebf204b1a8d87
Shaun Wang <spxwang@gmail.com>
2021-08-25 19:20:40 +1200

    fmt

--- benchmarking/src/lib.rs
+++ benchmarking/src/lib.rs
@@ -618,6 +618,7 @@ macro_rules! benchmark_backend {
 // Every variant must implement [`BenchmarkingSetup`].
 //
 // ```nocompile
+// 
 // struct Transfer;
 // impl BenchmarkingSetup for Transfer { ... }
 //
