61e7e27923e3d9e5bfdf19438be9f30e6d653994
Shaun Wang <spxwang@gmail.com>
2022-01-07 16:06:26 +1300

    XCM: add deposit error handler for multi-currency adapter. (#675)
    * XCM: add deposit error handler for multi-currency adapter.
    
    * Deposit to alternative account.
    
    * Make clippy happy.

--- xcm-support/src/lib.rs
+++ xcm-support/src/lib.rs
@@ -18,7 +18,7 @@ use xcm_executor::traits::{FilterAssetLocation, MatchesFungible};
 
 use orml_traits::location::Reserve;
 
-pub use currency_adapter::MultiCurrencyAdapter;
+pub use currency_adapter::{DepositToAlternative, MultiCurrencyAdapter, OnDepositFail};
 
 mod currency_adapter;
 
