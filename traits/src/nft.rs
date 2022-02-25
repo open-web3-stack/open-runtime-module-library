e85117ae6d193360471db131d5b26c9548a72ef7
Roy Yang <roy@laminar.one>
2022-02-25 17:22:36 +1300

    Added additional functions required to create an NFT class

--- traits/src/nft.rs
+++ traits/src/nft.rs
@@ -20,6 +20,17 @@ pub trait InspectExtended<AccountId>: Inspect<AccountId> {
 
 // Supplement trait to the nonfungibles::Create trait
 pub trait CreateExtended<AccountId, ClassProperties>: Create<AccountId> {
+	/// The balance of account.
+	type Balance: AtLeast32BitUnsigned + FullCodec + Copy + MaybeSerializeDeserialize + Debug + Default;
+
+	// Returns the next class ID to be created.
 	fn next_class_id() -> Self::ClassId;
+	// Sets the property of the given class.
 	fn set_class_properties(class: &Self::ClassId, properties: ClassProperties) -> DispatchResult;
+	// Pays for the fee to mint tokens of a particular class.
+	fn pay_mint_fee(payer: &AccountId, class: &Self::ClassId, quantity: u32) -> DispatchResult;
+	// Gets the base cost of minting one token for a class with default attributes.
+	fn base_mint_fee() -> Self::Balance;
+	// Gets the base cost of creating a new Class with default attributes.
+	fn base_create_class_fee() -> Self::Balance;
 }
