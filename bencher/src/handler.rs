7b6c226f4d94b9510e1ca8b1c2cfc55b8fc59680
zjb0807 <zjb0807@qq.com>
2022-05-07 23:41:30 +0800

    fix clippy

--- bencher/src/handler.rs
+++ bencher/src/handler.rs
@@ -22,7 +22,7 @@ struct BenchData {
 pub fn handle(output: Vec<u8>, storage_infos: Vec<StorageInfo>) {
 	println!();
 
-	let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap_or_default().replace("-", "_");
+	let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap_or_default().replace('-', "_");
 
 	let results = <Vec<BenchResult> as Decode>::decode(&mut &output[..]).unwrap();
 	let data: Vec<BenchData> = results
@@ -65,12 +65,12 @@ pub fn handle(output: Vec<u8>, storage_infos: Vec<StorageInfo>) {
 					"{:?}",
 					Duration::from_nanos(model.parameters.intercept_value as u64)
 				)),
-				format!(
+				green_bold(&format!(
 					"tracked: [r: {}, w: {}]",
-					green_bold(&total_reads.to_string()),
-					green_bold(&total_writes.to_string())
-				),
-				format!("total: [r: {}, w: {}]", result.reads, result.writes)
+					&total_reads.to_string(),
+					&total_writes.to_string()
+				)),
+				green_bold(&format!("total: [r: {}, w: {}]", result.reads, result.writes))
 			);
 
 			BenchData {
