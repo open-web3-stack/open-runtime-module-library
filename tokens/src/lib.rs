8f0a4ee1763c22969abbcefbd1585ef187eb34bd
zqhxuyuan <zqhxuyuan@gmail.com>
2022-05-12 11:01:20 +0800

    refactor tokens tests (#735)
    * split tokens tests
    
    * fmt

--- tokens/src/lib.rs
+++ tokens/src/lib.rs
@@ -75,6 +75,11 @@ mod imbalances;
 mod impls;
 mod mock;
 mod tests;
+mod tests_currency_adapter;
+mod tests_events;
+mod tests_fungibles;
+mod tests_multicurrency;
+
 mod weights;
 
 pub use impls::*;
