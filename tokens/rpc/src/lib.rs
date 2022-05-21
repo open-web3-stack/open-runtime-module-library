397cee177102580316c6e2b9bb8ade6165dab43d
zqh <zqhxuyuan@gmail.com>
2022-05-22 00:52:16 +0800

    fmt

--- tokens/rpc/src/lib.rs
+++ tokens/rpc/src/lib.rs
@@ -55,7 +55,8 @@ impl From<Error> for i32 {
 }
 
 #[async_trait]
-impl<C, Block, CurrencyId, Balance> TokensApiServer<<Block as BlockT>::Hash, CurrencyId, Balance> for TokensRpc<C, Block>
+impl<C, Block, CurrencyId, Balance> TokensApiServer<<Block as BlockT>::Hash, CurrencyId, Balance> 
+	for TokensRpc<C, Block>
 where
 	Block: BlockT,
 	C: ProvideRuntimeApi<Block> + HeaderBackend<Block> + Send + Sync + 'static,
@@ -89,6 +90,6 @@ where
 			})
 		};
 
-		try_into_rpc_balance(balance?)
+		try_into_rpc_balance(balance)?
 	}
 }
