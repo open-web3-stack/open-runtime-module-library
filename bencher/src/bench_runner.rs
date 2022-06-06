0c0e3a855058822e6f26c2df1601b24794580705
zqh <zqhxuyuan@gmail.com>
2022-06-07 00:21:11 +0800

    update polkadot-v0.9.23

--- bencher/src/bench_runner.rs
+++ bencher/src/bench_runner.rs
@@ -3,7 +3,7 @@ use super::{
 	tracker::{BenchTracker, BenchTrackerExt},
 };
 use frame_benchmarking::frame_support::sp_runtime::traits::Block;
-use sc_executor::{WasmExecutionMethod, WasmExecutor};
+use sc_executor::{WasmExecutionMethod, WasmExecutor, WasmtimeInstantiationStrategy};
 use sc_executor_common::runtime_blob::RuntimeBlob;
 use sp_externalities::Extensions;
 use sp_state_machine::{Ext, OverlayedChanges, StorageTransactionCache};
@@ -31,7 +31,9 @@ pub fn run<B: Block>(wasm_code: Vec<u8>) -> std::result::Result<Vec<u8>, sc_exec
 	let mut bench_ext = BenchExt::new(ext, tracker);
 
 	let executor =
-		WasmExecutor::<ComposeHostFunctions>::new(WasmExecutionMethod::Compiled, Default::default(), 1, None, 1);
+		WasmExecutor::<ComposeHostFunctions>::new(WasmExecutionMethod::Compiled{
+			instantiation_strategy: WasmtimeInstantiationStrategy::PoolingCopyOnWrite
+		}, Default::default(), 1, None, 1);
 
 	let blob = RuntimeBlob::uncompress_if_needed(&wasm_code[..]).unwrap();
 
