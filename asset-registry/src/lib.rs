1908d7f81165d5b4ce9fe62a5bd707af93049f36
nuno <hi@nunoalexandre.com>
2022-06-13 14:37:27 +0200

    asset-registry: pub use AssetMetadata
    Doing so makes logical sense and smoothes the update to this new
    version.

--- asset-registry/src/lib.rs
+++ asset-registry/src/lib.rs
@@ -4,7 +4,8 @@
 #![allow(clippy::large_enum_variant)]
 use frame_support::{pallet_prelude::*, traits::EnsureOriginWithArg, transactional};
 use frame_system::pallet_prelude::*;
-use orml_traits::asset_registry::{AssetMetadata, AssetProcessor};
+pub use orml_traits::asset_registry::AssetMetadata;
+use orml_traits::asset_registry::AssetProcessor;
 use scale_info::TypeInfo;
 use sp_runtime::{
 	traits::{AtLeast32BitUnsigned, Member},
