1c37ce28144ac886bf4db41ad69ada491131e90e
Striker <siddiquishoeb.iitm@gmail.com>
2022-06-19 15:08:07 +0530

    MPL - pre-compilation commit

--- tokens/src/multi_token_currency.rs
+++ tokens/src/multi_token_currency.rs
@@ -361,4 +361,5 @@ pub trait MultiTokenCurrencyExtended<AccountId>: MultiTokenCurrency<AccountId> {
 	fn exists(currency_id: Self::CurrencyId) -> bool;
 	fn burn_and_settle(currency_id: Self::CurrencyId, who: &AccountId, amount: Self::Balance) -> DispatchResult;
 	fn locked_balance(currency_id: Self::CurrencyId, who: &AccountId) -> Self::Balance;
+	fn available_balance(currency_id: Self::CurrencyId, who: &AccountId) -> Self::Balance;
 }
