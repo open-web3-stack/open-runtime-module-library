33dbc5e35305d0cf5937c896dae8655ca7da95d8
zqh <zqhxuyuan@gmail.com>
2022-07-17 22:41:18 +0800

    clear_prefix replace remove_prefix

--- oracle/src/lib.rs
+++ oracle/src/lib.rs
@@ -231,8 +231,7 @@ impl<T: Config<I>, I: 'static> ChangeMembers<T::AccountId> for Pallet<T, I> {
 	fn change_members_sorted(_incoming: &[T::AccountId], outgoing: &[T::AccountId], _new: &[T::AccountId]) {
 		// remove values
 		for removed in outgoing {
-			#[allow(deprecated)]
-			RawValues::<T, I>::remove_prefix(removed, None);
+			let _ = RawValues::<T, I>::clear_prefix(removed, u32::MAX, None);
 		}
 	}
 
